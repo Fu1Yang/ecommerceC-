a3aa99619199f96c07bbc5aca817521984dba065
Fu1Yang <yang.fu3785@gmail.com>
2025-12-04 20:05:11 +0100

    feat Add Api connexion account Users

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ecommerce.Data;
+using ecommerce.Models;
+using ecommerce.Pages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ecommerce.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly DataContext _context;
+        private string email;
+        private string password;
+
+        public UserController(DataContext context)
+        {
+            _context = context; // OBLIGATOIRE
+        }
+
         // GET: api/<UserController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -23,9 +37,38 @@ namespace ecommerce.Controllers
         }
 
         // POST api/<UserController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpPost("infoUser")]
+        public async Task<ActionResult<string>> InfoUser([FromBody] LoginRequest request)
         {
+            if (request == null)
+                return BadRequest(new LoginResponse
+                {
+                    Success = false,
+                    Message = "Requete invalide"
+                });
+
+            if (string.IsNullOrEmpty(request.Id) || string.IsNullOrEmpty(request.Password))
+            {
+                return BadRequest(new LoginResponse { Success = false, Message = "Champs vides" });
+            }
+            //En 1 On cherche l'utilisateur par email
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == request.Id);
+
+            if (user == null)
+                return Unauthorized(new LoginResponse { Success = false, Message = "Email incorrect" });
+            //En 2 Vérification mot de passe avec hashing
+            bool ValidPassword = BCrypt.Net.BCrypt.Verify(request.Password, user.Password);
+
+            if (!ValidPassword)
+                return Unauthorized(new LoginResponse { Success = false, Message = "Mot de passe incorrect" });
+
+            // Connexion OK
+            return Ok(new LoginResponse
+            {
+                Success = true,
+                Message = "Connexion réussie"
+            });
         }
 
         // PUT api/<UserController>/5
--- DTO/UsersDto.cs
+++ DTO/UsersDto.cs
@@ -2,5 +2,11 @@
 {
     public class UsersDto
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Role { get; set; }
     }
 }
