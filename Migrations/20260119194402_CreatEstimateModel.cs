379605f53b236d87bbf1df8f7c09304937cab7b3
Yang Fu <yang.fu3785@gmail.com>
2026-01-19 20:46:54 +0100

    [FEAT]: Estimate > creation of the estimate model"; ;

---
+++ Migrations/20260119194402_CreatEstimateModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatEstimateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estimate",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telephone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Adresse = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TarifTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estimate", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estimate");
+        }
+    }
+}
---
+++ Models/Estimate.cs
@@ -0,0 +1,25 @@
+﻿namespace ecommerce.Models
+{
+    public class Estimate
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public string Adresse { get; set; }
+        public decimal TarifTotal { get; set; }
+
+        public Estimate() { }
+
+        public Estimate(int id, string nom, string email, string telephone, string adresse, decimal tarifTotal)
+        {
+            Id = id;
+            Nom = nom;
+            Email = email;
+            Telephone = telephone;
+            Adresse = adresse;
+            TarifTotal = tarifTotal;
+        }
+    }
+
+}
