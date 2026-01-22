aebf5cf3ff0e5dd3b6c16a9e2406a0d13b7e7c70
Fu1Yang <yang.fu3785@gmail.com>
2026-01-22 20:02:03 +0100

    update of the estimate dto  code

--- DTO/EstimateDto.cs
+++ DTO/EstimateDto.cs
@@ -6,15 +6,22 @@ namespace ecommerce.DTO
     {
         [Required]
         public string Nom { get; set; } = "";
+
         [EmailAddress]
         public string Email { get; set; } = "";
+
         [Required]
         public string Telephone { get; set; } = "";
+
         [Required]
         public string Adresse { get; set; } = "";
 
+        [Required]
+        public string Intervention { get; set; }
+
         [Range(0, 10000)]
         public decimal TarifTotal { get; set; }
+
         public DateTime Date { get; set; }
     }
 }
